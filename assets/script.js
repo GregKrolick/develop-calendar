98e97d4561221e5de4bac1906c4f98166e796b6c
Greg Krolick <gregrobk@live.com>
2022-10-06 22:37:54 -0400

    add icon to save buttons

--- assets/script.js
+++ assets/script.js
@@ -27,4 +27,28 @@ $('.time-block').each(function(){
       }
     })
 
+
+// pulls from local storage previously saved descriptions
+$("#9 .description").val(localStorage.getItem("9AM"));
+$("#10 .description").val(localStorage.getItem("10AM"));
+$("#11 .description").val(localStorage.getItem("11AM"));
+$("#12 .description").val(localStorage.getItem("12PM"));
+$("#13 .description").val(localStorage.getItem("1PM"));
+$("#14 .description").val(localStorage.getItem("2PM"));
+$("#15 .description").val(localStorage.getItem("3PM"));
+$("#16 .description").val(localStorage.getItem("4PM"));
+$("#17 .description").val(localStorage.getItem("5PM"));
+
+
+// saves description when save button is pressed
+$(".saveBtn").on("click", function (event) {
+       
+    var time = $(this).siblings(".hour").text();
+    var text = $(this).siblings(".description").val();
+    
+    localStorage.setItem(time, text);
+})
+
+
+//add to have colors change when hour changes
 setInterval(displayDate, 1000);
